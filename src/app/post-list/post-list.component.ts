04a8d3c0db5c8dc7353d1e86cbcdfd2cb52b785e
Aka Djé <jerome.bataille@protonmail.com>
2018-10-14 19:57:14 +0200

    version sans l'arry dans l'app principal

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,32 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
 })
-export class AppComponent {
-  title = 'OCR-blog-Anguar-Project';
-  /*
+export class PostListComponent implements OnInit {
+
+  //@Input() posts[] : array;
+
   posts = [
     {
       title: "Mon premier post",
       content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       loveIts: 0,
-      created_at: Date
+      created_at: new Date()
     },
     {
       title: "Mon deuxième post",
       content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       loveIts: 0,
-      created_at: Date
+      created_at: new Date()
     },
     {
       title: "Un autre post",
       content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       loveIts: 0,
-      created_at: Date
+      created_at: new Date()
     }
   ]
-  */
+/*
+  @Input() postTitle : string;
+  @Input() postContent : string;
+  @Input() postLoveIts : number;
+  @Input() postDate : Date;
+*/
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
